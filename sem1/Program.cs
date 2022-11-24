a329f871a662b74ae7771c1377f85cc8fb2b5885
Daria <dashavolodina@gmail.com>
2022-11-24 23:22:15 +0800

    Рабочая версия нахождение максимального среди двух чисел введеных пользователем

---
+++ sem1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Давайте выясним какое число больше..");
+
+Console.Write("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if( num1 < num2)
+{
+    max= num2;
+}
+else
+{
+    max= num1;
+}
+Console.Write("Наибольшим является: ");
+Console.WriteLine(max);
