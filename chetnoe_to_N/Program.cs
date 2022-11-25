0fd181b4f99572be219b02137a17068343234786
Daria <dashavolodina@gmail.com>
2022-11-25 22:05:48 +0800

    Рабочая версия нахождения всех четных чисел до N

---
+++ chetnoe_to_N/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Введите целое цисло: ");
+
+        int num = Convert.ToInt32(Console.ReadLine());
+        int num2 = 1;
+       
+        if (num2 == 1)
+
+        {
+         num2 += 1;
+        }
+
+        while (num2 <= num)
+
+        {
+            Console.Write("Все четные числа до вашего: ");
+            Console.WriteLine(num2);
+            num2 += 2;
+        }
